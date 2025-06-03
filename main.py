c5a789a72b1293db3d9f68f2889e7d56846a0f39
Luciano Martínez <lucianomartinez27@gmail.com>
2025-06-03 14:58:42 -0300

    Llamar funciones de operaciones con años

--- main.py
+++ main.py
@@ -98,27 +98,10 @@ def calcular_predominio_digitos_altos(conjunto):
     else:
         print(f"{conjunto} es un conjunto predominio de dígitos bajos")
 
-if __name__ == "__main__":
-    dni_luciano = "38262759"
-    dni_santiago = "40679033"
-    conjunto1 = convertir_a_conjunto(dni_luciano)
-    conjunto2 = convertir_a_conjunto(dni_santiago)
-    calculo_y_visualizacion(conjunto1, conjunto2)
-    print("-" * 20)
-    print("Conteo Luciano: ", json.dumps(conteo_de_frecuencia(dni_luciano), indent=4))
-    print("Conteo Santiago: ", json.dumps(conteo_de_frecuencia(dni_santiago), indent=4))
-
-    print("-" * 20)
-    print("Si algún dígito aparece en todos los conjuntos, se marca como dígito común:")
-    digitos_comunes(conjunto1, conjunto2)
-    print("-" * 20)
-    print("Si conjunto tiene más números mayores o iguales a 5 que menores a 5 se marca como conjunto con predominio de dígitos altos")
-    calcular_predominio_digitos_altos(conjunto1)
-    calcular_predominio_digitos_altos(conjunto2)
 
 #//////////////////////// anios /////////////////////
 
-lista_anios = [1995, 1997]
+lista_anios = [1994, 1997]
 
 # Contar cuántos nacieron en años pares e impares utilizando estructuras repetitivas.
 def anio_par_impar(anios):
@@ -135,7 +118,7 @@ def anio_par_impar(anios):
     print(f'Nacidos en años impares: {impares}')
 
 # Si todos nacieron después del 2000, mostrar "Grupo Z".
-def grupo_z(anio):
+def grupo_z():
     todos_son_mayores = True
     for anio in lista_anios:
         if anio <= 2000:
@@ -147,7 +130,7 @@ def grupo_z(anio):
 
 # Calcular el producto cartesiano entre el conjunto de años y el conjunto de edades actuales.
 def producto_cartesiano(anios):
-    edades = [29, 27]
+    edades = [30, 27]
 
     cartesiano = []
 
@@ -167,3 +150,29 @@ def verificar_anios_bisiestos(lista_anios):
         print("Tenemos un año especial")
     else:
         print("Ningún año es bisiesto")
+
+
+if __name__ == "__main__":
+    dni_luciano = "38262759"
+    dni_santiago = "40679033"
+
+    print("-" * 10, " A. Operaciones con DNIs ", "-" *10)
+    conjunto1 = convertir_a_conjunto(dni_luciano)
+    conjunto2 = convertir_a_conjunto(dni_santiago)
+    calculo_y_visualizacion(conjunto1, conjunto2)
+    print("-" * 20)
+    print("Conteo Luciano: ", json.dumps(conteo_de_frecuencia(dni_luciano), indent=4))
+    print("Conteo Santiago: ", json.dumps(conteo_de_frecuencia(dni_santiago), indent=4))
+
+    print("-" * 20)
+    print("Si algún dígito aparece en todos los conjuntos, se marca como dígito común:")
+    digitos_comunes(conjunto1, conjunto2)
+    print("-" * 20)
+    print("Si conjunto tiene más números mayores o iguales a 5 que menores a 5 se marca como conjunto con predominio de dígitos altos")
+    calcular_predominio_digitos_altos(conjunto1)
+    calcular_predominio_digitos_altos(conjunto2)
+
+    print("-" * 10, " B. Operaciones con años de nacimiento ", "-" * 10)
+    producto_cartesiano(lista_anios)
+    verificar_anios_bisiestos(lista_anios)
+    grupo_z()
